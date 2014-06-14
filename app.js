6155f59a8adb5a4b33854bcd89a07c738eeef6f8
lizzb <huzzah.its.lizz@gmail.com>
2014-06-14 15:28:15 -0500

    generates an iday specific layout on start

--- app.js
+++ app.js
@@ -38,8 +38,685 @@ angular.module('app', ['flowChart', ])
 
 
 	var chartDataModel = {
+
+    "nodes": [
+        {
+            "name": "fenwal",
+            "id": "booth1",
+            "x": 824,
+            "y": 620,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "carsdotcom",
+            "id": "booth2",
+            "x": 698,
+            "y": 620,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "fti",
+            "id": "booth3",
+            "x": 570,
+            "y": 620,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "bemis",
+            "id": "booth4",
+            "x": 443,
+            "y": 620,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "epic",
+            "id": "booth5",
+            "x": 315,
+            "y": 620,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "mathworks",
+            "id": "booth6",
+            "x": 188,
+            "y": 620,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "boeing",
+            "id": "booth7",
+            "x": 64,
+            "y": 620,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "kiewit",
+            "id": "booth8",
+            "x": 11,
+            "y": 550.0000228881836,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "alcatel-lucent",
+            "id": "booth9",
+            "x": 45,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "imc",
+            "id": "booth10",
+            "x": 170,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "mwrdgc",
+            "id": "booth11",
+            "x": 293,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "cme-group",
+            "id": "booth12",
+            "x": 418,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "pratt-whitney",
+            "id": "booth13",
+            "x": 545,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "gtl",
+            "id": "booth14",
+            "x": 670,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "pwc",
+            "id": "booth15",
+            "x": 797,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "itw",
+            "id": "booth16",
+            "x": 944,
+            "y": 450,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "trading-tech",
+            "id": "booth17",
+            "x": 944,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "sbb",
+            "id": "booth18",
+            "x": 797,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "outerwall",
+            "id": "booth19",
+            "x": 670,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "com-ed",
+            "id": "booth20",
+            "x": 543,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "chopper-trading",
+            "id": "booth21",
+            "x": 416,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "citadel",
+            "id": "booth22",
+            "x": 291,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "clarity",
+            "id": "booth23",
+            "x": 170,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "yahoo",
+            "id": "booth24",
+            "x": 45,
+            "y": 400,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "gdeb",
+            "id": "booth25",
+            "x": 36,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "wolverine-trading",
+            "id": "booth26",
+            "x": 163,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "mta",
+            "id": "booth27",
+            "x": 288,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "mbhb-llc",
+            "id": "booth28",
+            "x": 413,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "think-big",
+            "id": "booth29",
+            "x": 538,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "here-nokia",
+            "id": "booth30",
+            "x": 663,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "etherios",
+            "id": "booth31",
+            "x": 788,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "lend-lease",
+            "id": "booth32",
+            "x": 933,
+            "y": 325,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "schneider-electric",
+            "id": "booth33",
+            "x": 932,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "zebra-tech",
+            "id": "booth34",
+            "x": 779,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "ucc",
+            "id": "booth35",
+            "x": 650,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "northwestern-mutual",
+            "id": "booth36",
+            "x": 519,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "idot",
+            "id": "booth37",
+            "x": 390,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "pariveda",
+            "id": "booth38",
+            "x": 269,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "continental",
+            "id": "booth39",
+            "x": 138,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "zs-associates",
+            "id": "booth40",
+            "x": 11,
+            "y": 275,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "ida",
+            "id": "booth41",
+            "x": 1118,
+            "y": 385,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "factset",
+            "id": "booth42",
+            "x": 1113,
+            "y": 333,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "altec",
+            "id": "booth43",
+            "x": 1114,
+            "y": 277,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "chrylser-group",
+            "id": "booth44",
+            "x": 1115,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "adage-tech",
+            "id": "booth45",
+            "x": 982,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "denso",
+            "id": "booth46",
+            "x": 863,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "environ",
+            "id": "booth47",
+            "x": 738,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "ama",
+            "id": "booth48",
+            "x": 621,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "wrigley",
+            "id": "booth49",
+            "x": 492,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "sapient-nitro",
+            "id": "booth50",
+            "x": 365,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "hfz-llc",
+            "id": "booth51",
+            "x": 246,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "avantcredit",
+            "id": "booth52",
+            "x": 125,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "spacex",
+            "id": "booth53",
+            "x": 2,
+            "y": 180,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "dmc-inc",
+            "id": "booth54",
+            "x": 891,
+            "y": 1224,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "pepsico",
+            "id": "booth55",
+            "x": 782,
+            "y": 1140,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "wms",
+            "id": "booth56",
+            "x": 645,
+            "y": 1142,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "nielsen",
+            "id": "booth57",
+            "x": 514,
+            "y": 1141,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "dropbox",
+            "id": "booth58",
+            "x": 377,
+            "y": 1141,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "thoughtworks",
+            "id": "booth59",
+            "x": 228,
+            "y": 1173,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "allstate",
+            "id": "booth60",
+            "x": 229,
+            "y": 1223,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "deloitte",
+            "id": "booth61",
+            "x": 456,
+            "y": 1277,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "oracle",
+            "id": "booth62",
+            "x": 581,
+            "y": 1277,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "ge",
+            "id": "booth63",
+            "x": 706,
+            "y": 1277,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "mozilla",
+            "id": "booth64",
+            "x": 901,
+            "y": 964.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "tata",
+            "id": "booth65",
+            "x": 26,
+            "y": 1009.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "hospira",
+            "id": "booth66",
+            "x": 151,
+            "y": 1009.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "pec",
+            "id": "booth67",
+            "x": 276,
+            "y": 1009.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "readyforce",
+            "id": "booth68",
+            "x": 401,
+            "y": 1009.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "accenture",
+            "id": "booth69",
+            "x": 526,
+            "y": 1009.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "shmoop",
+            "id": "booth70",
+            "x": 651,
+            "y": 1009.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "ford",
+            "id": "booth71",
+            "x": 776,
+            "y": 1009.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "united-airlines",
+            "id": "booth72",
+            "x": 900,
+            "y": 727.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "goodyear",
+            "id": "booth73",
+            "x": 25,
+            "y": 772.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "at-t",
+            "id": "booth74",
+            "x": 150,
+            "y": 772.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "s-c-electric",
+            "id": "booth75",
+            "x": 275,
+            "y": 772.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "exxon-mobil",
+            "id": "booth76",
+            "x": 400,
+            "y": 772.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "facebook",
+            "id": "booth77",
+            "x": 525,
+            "y": 772.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "discover",
+            "id": "booth78",
+            "x": 650,
+            "y": 772.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "3m",
+            "id": "booth79",
+            "x": 775,
+            "y": 772.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "baxter",
+            "id": "booth80",
+            "x": 527,
+            "y": 867.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "p-g",
+            "id": "booth81",
+            "x": 27,
+            "y": 866.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "northrop-grumman",
+            "id": "booth82",
+            "x": 152,
+            "y": 866.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "gaf",
+            "id": "booth83",
+            "x": 277,
+            "y": 866.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        },
+        {
+            "name": "microsoft",
+            "id": "booth84",
+            "x": 402,
+            "y": 866.0000305175781,
+            "inputConnectors": [],
+            "outputConnectors": []
+        }
+    ],
+    "connections": []
+};
 	
-		nodes: [
+		/*nodes: [
 			{
 				name: "Example Node 1",
 				id: 0,
@@ -61,7 +738,9 @@ angular.module('app', ['flowChart', ])
 		],
 
 		connections: []
-	};
+	};*/
+
+
 	
 	//var floorplanDataModel
 	/*var chartDataModel = {
@@ -378,6 +1057,9 @@ companyNameIDs.push("microsoft");
 		var numRows = parseInt(numRowInput);
 		var numCols = parseInt(numColInput);
 		*/
+
+
+		/// i need a ltircky trick here for error checking
 
 		var numRows = 11; //companyNameIDs.length % 2;
 		var numCols = 8; //companyNameIDs.length;
